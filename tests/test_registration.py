6a8e15e06e04b5e557fb72c3db21e8ee4e2f2fe4
VeronikaMukhtarova <veronika.mukhtarova88@yandex.ru>
2024-10-26 20:09:08 +0300

    rename_test_name

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,28 +1,10 @@
-from playwright.sync_api import Page, expect
+import pytest
+from playwright.sync_api import Page
 from pages.registration_page import RegistrationPage
 
 def test_add_todo(page:Page):
     registration_page = RegistrationPage(page)
-    registration_page.navigate()
+    registration_page.open()
+    registration_page.firstname_click()
+    registration_page.firstname_fill()
     registration_page.firstname_enter()
-    registration_page.lastname_enter()
-    registration_page.email_enter()
-    registration_page.female_click()
-    registration_page.mobile_enter()
-    registration_page.data_of_birthday_enter()
-    registration_page.subject_enter()
-    registration_page.hobbies_check()
-    registration_page.select_picture_enter()
-    registration_page.address_enter()
-    registration_page.select_state_click()
-    registration_page.select_city_click()
-    registration_page.submit_click()
-
-
-
-
-
-
-
-
-
