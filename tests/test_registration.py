6948a751ea07e6193b651d2ecc767e1b97685255
VeronikaMukhtarova <veronika.mukhtarova88@yandex.ru>
2024-10-26 20:04:24 +0300

    rename_test_name

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,5 +1,4 @@
-import pytest
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 from pages.registration_page import RegistrationPage
 
 def test_add_todo(page:Page):
